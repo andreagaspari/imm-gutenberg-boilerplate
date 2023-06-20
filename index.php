d3a2395e3161c1e2144d846567bc1b8cfa57cce9
andreagaspari <andrea.g@immaginificio.com>
2023-06-20 22:44:42 +0200

    First test

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Registers all block assets so that they can be enqueued through Gutenberg in
+ * the corresponding context.
+ *
+ * Passes translations to JavaScript.
+ */
+ 
+function boilerplate_register_block() {
+	if ( ! function_exists( 'register_block_type' ) ) {
+		// Gutenberg is not active.
+		return;
+	}
+	
+	// Register the block by passing the location of block.json to register_block_type.
+	register_block_type( __DIR__ );
+
+	if ( function_exists( 'wp_set_script_translations' ) ) {
+		wp_set_script_translations( 'boilerplate', LANG_DOMAIN );
+	}
+
+}
+add_action( 'init', 'boilerplate_register_block' );
